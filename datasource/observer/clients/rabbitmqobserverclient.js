a14d95966a3c3b37e8e69f9b71784b241a1d2a3a
Rodrigo Costa <rodrigo.costa@ibm.com>
2020-03-27 18:20:39 -0300

    Improve log messages

--- datasource/observer/clients/rabbitmqobserverclient.js
+++ datasource/observer/clients/rabbitmqobserverclient.js
@@ -74,7 +74,8 @@ class RabbitMQObserverClient extends ObserverClient
 			const q = await this._channel.assertQueue ('', {exclusive: true});
 			this._channel.bindQueue (q.queue, this._exchangeName, '');
 
-			console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+			console.log(`Bound to exchange "${this._exchangeName}"`);
+			console.log(" [*] Waiting for messages in %s.", q.queue);
 
 			this._channel.consume (q.queue,
 				(msg) =>
