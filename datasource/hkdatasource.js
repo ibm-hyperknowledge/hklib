a024de46cef3024c8c7039b3ec5649c6c32bf332
Marcelo Machado <marcelo.machado@ibm.com>
2020-10-05 14:00:18 -0300

    Adding info function to access the info endpoint from hkbase.

--- datasource/hkdatasource.js
+++ datasource/hkdatasource.js
@@ -78,6 +78,46 @@ function convertEntities(raw)
 }
 
 /**
+ * @callback OperationCallback
+ * @param err An error object that indicate if the operation was succesful or not
+ */
+/**
+ * Get information about the current IDB
+ *
+ * @param {OperationCallback} callback Response callback
+ */
+
+HKDatasource.prototype.getInfo = function (callback = () => {})
+{
+	request.get(`${this.url}/info`, this.options, (err, res) =>
+	{
+		if(!err)
+		{
+			if(requestCompletedWithSuccess (res.statusCode))
+			{
+				try
+				{
+					let data = JSON.parse(res.body);
+					callback(null, data);
+				}
+				catch(exp)
+				{
+					callback(exp);
+				}
+			}
+			else
+			{
+				callback(`Server responded with ${res.statusCode}. ${res.body}`);
+			}
+		}
+		else
+		{
+			callback(err);
+		}
+	});
+}
+
+/**
  * Callback function for `addEntities`
  *
  * @callback ListRepositoriesCallback
