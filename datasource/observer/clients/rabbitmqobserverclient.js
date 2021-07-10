b1b69c99c5a3a6f77ef15ddf53793e0a43994f81
Elton Soares <eltons@ibm.com>
2021-07-09 23:52:24 -0300

    Reviewing use of amqp-connection-manager to automatically reconnect and buffer messages when rabbitmq broker connection gets lost

--- datasource/observer/clients/rabbitmqobserverclient.js
+++ datasource/observer/clients/rabbitmqobserverclient.js
@@ -6,21 +6,19 @@
 'use strict';
 
 const ObserverClient = require ('./observerclient')
-const amqp           = require ('amqplib');
+const amqp           = require ('amqp-connection-manager');
 
 async function createChannel ()
 {
 	try
 	{
-		this._channel = await this._connection.createChannel ({
-			json: true,
-			setup: (channel) => {
-				return channel.assertQueue(this._exchangeName, this._exchangeOptions);
-			}
+		this._channelWrapper = this._connectionManager.createChannel ();
+		this._channelWrapper.addSetup((channel) =>
+		{
+			channel.assertQueue(this._exchangeName, this._exchangeOptions);
+			channel.on ('error', console.log);
+			channel.on ('close', () => this.init());
 		});
-		this._channel.on ('error', console.log);
-		this._channel.on ('close', () => this.init() );
-
 		return Promise.resolve ();
 	}
 	catch (err)
@@ -40,15 +38,16 @@ async function connect ()
 			options.ca = [Buffer.from (this._certificate, 'base64')];
 		}
 
-		this._connection = await amqp.connect (this._broker, options);
-		this._connection.on ('error', console.log);
+		this._connectionManager = amqp.connect (this._broker, options);
+		await this._connectionManager._connectPromise;
+		this._connectionManager._currentConnection.connection.on ('error', console.log);
 
 		await createChannel.call(this);
 	}
 	catch (err)
 	{
 		console.log(err);
-		this._connection = null;
+		this._connectionManager = null;
 		return Promise.reject(err);
 	}
 }
@@ -59,11 +58,12 @@ class RabbitMQObserverClient extends ObserverClient
 	constructor (info, options)
 	{
 		super ();
-		this._broker          = info.broker;
-		this._exchangeName    = info.exchangeName;
-		this._exchangeOptions = info.exchangeOptions;
-		this._certificate     = info.certificate || options.certificate;
-		this._connection      = null;
+		this._broker            = info.broker;
+		this._exchangeName      = info.exchangeName;
+		this._exchangeOptions   = info.exchangeOptions;
+		this._certificate       = info.certificate || options.certificate;
+		this._connectionManager = null;
+		this._channelWrapper    = null;
 	}
 
 	static getType ()
@@ -76,18 +76,18 @@ class RabbitMQObserverClient extends ObserverClient
 		try
 		{
 			await connect.call (this);
-			const q = await this._channel.assertQueue ('', {exclusive: true});
-			this._channel.bindQueue (q.queue, this._exchangeName, '');
+			this._channelWrapper.addSetup(async (channel) =>
+			{
+				const q = await channel.assertQueue ('', {exclusive: true});
+				channel.bindQueue (q.queue, this._exchangeName, '');
+				console.log(`Bound to exchange "${this._exchangeName}"`);
+				console.log(" [*] Waiting for messages in %s.", q.queue);
 
-			console.log(`Bound to exchange "${this._exchangeName}"`);
-			console.log(" [*] Waiting for messages in %s.", q.queue);
-
-			this._channel.consume (q.queue,
-				(msg) =>
+				channel.consume (q.queue, (msg) =>
 				{
 					try
 					{
-						let notification = JSON.parse(JSON.parse (msg.content.toString()));
+						let notification = JSON.parse (msg.content.toString());
 						this.notify (notification);
 					}
 					catch (err)
@@ -95,7 +95,8 @@ class RabbitMQObserverClient extends ObserverClient
 						console.log (err);
 					}
 				}, {noAck: true});
-		}
+			});
+		}			
 		catch (err)
 		{
 			console.log(err);
