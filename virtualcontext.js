36b46fd070a77c8682c7dde3a334b7ac165a3915
Marcelo Machado <marcelo.oc.machado@gmail.com>
2021-10-27 01:17:18 -0300

    Dealing with virtual context.

--- virtualcontext.js
+++ virtualcontext.js
@@ -6,7 +6,6 @@
 "use strict";
 
 const Types = require("./types");
-const HKEntity = require("./hkentity");
 const Context = require("./context");
 
 function VirtualContext(id, src, parent=null)
@@ -21,14 +20,43 @@ function VirtualContext(id, src, parent=null)
 VirtualContext.prototype = Object.create(Context.prototype);
 VirtualContext.prototype.constructor = VirtualContext;
 
+VirtualContext.prototype.serialize = function()
+{
+	let virtualContext = {
+		id: this.id,
+		type: Types.VIRTUALCONTEXT,
+		parent: this.parent || null
+	}
+
+	if (this.properties)
+	{
+		virtualContext.properties = this.serializeProperties();
+	}
+
+	if (this.metaProperties)
+	{
+		virtualContext.metaProperties = this.serializeMetaProperties();
+	}
+
+	if (this.interfaces)
+	{
+		virtualContext.interfaces = {};
+		for (let k in this.interfaces)
+		{
+			virtualContext.interfaces[k] = JSON.parse(JSON.stringify(this.interfaces[k])) // Lazy job
+		}
+	}
+
+	return virtualContext;
+}
+
 function isValid(entity)
 {
 	let isValid = false;
 	if (entity && typeof(entity) === 'object' && !Array.isArray(entity))
 	{
 		if (entity.hasOwnProperty('type') && entity.type === Types.VIRTUALCONTEXT &&
-			entity.hasOwnProperty('id') && entity.hasOwnProperty('parent') &&
-			entity.hasOwnProperty('endpoint'))
+			entity.hasOwnProperty('id') && entity.hasOwnProperty('parent'))
 		{
 			isValid = true;
 		}
