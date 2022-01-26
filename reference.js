190bec261fdd06f1c367aefabd9e7e2cce351437
Sandro Rama Fiorini <srfiorini@users.noreply.github.com>
2022-01-26 17:18:37 -0300

    Reformated Reference node.

--- reference.js
+++ reference.js
@@ -11,74 +11,74 @@ const Node = require("./node");
 class Reference extends Node
 {
 
-constructor(id, refId, parent)
-{
-  super(id, parent);
-  if (arguments[0] && typeof arguments[0] === "object")
-  {
-    let ref = arguments[0];
-    this.id = ref.id || null;
-    this.ref = ref.ref || null;
-    this.parent = ref.parent || null;
-    if (ref.properties)
+    constructor(id, refId, parent)
     {
-      this.properties = ref.properties;
+        super(id, parent);
+        if (arguments[0] && typeof arguments[0] === "object")
+        {
+            let ref = arguments[0];
+            this.id = ref.id || null;
+            this.ref = ref.ref || null;
+            this.parent = ref.parent || null;
+            if (ref.properties)
+            {
+                this.properties = ref.properties;
+            }
+            if (ref.metaProperties)
+            {
+                this.metaProperties = ref.metaProperties;
+            }
+        }
+        else
+        {
+            this.id = id || null;
+            this.ref = refId;
+            this.parent = parent || null;
+        }
+        this.type = Types.REFERENCE;
     }
-    if (ref.metaProperties)
+
+
+    serialize = function ()
     {
-      this.metaProperties = ref.metaProperties;
+        let ref =
+        {
+            id: this.id,
+            type: Types.REFERENCE,
+            ref: this.ref,
+            parent: this.parent || null
+        }
+
+        if (this.properties)
+        {
+            ref.properties = this.serializeProperties();
+        }
+
+        if (this.metaProperties)
+        {
+            ref.metaProperties = this.serializeMetaProperties();
+        }
+
+        return ref;
     }
-  }
-  else
-  {
-    this.id = id || null;
-    this.ref = refId;
-    this.parent = parent || null;
-  }
-  this.type = Types.REFERENCE;
-}
 
-
-serialize = function ()
-{
-  let ref =
-  {
-    id: this.id,
-    type: Types.REFERENCE,
-    ref: this.ref,
-    parent: this.parent || null
-  }
-
-  if (this.properties)
-  {
-    ref.properties = this.serializeProperties();
-  }
-
-  if (this.metaProperties)
-  {
-    ref.metaProperties = this.serializeMetaProperties();
-  }
-
-  return ref;
-}
-
-static isValid(ref)
-{
-  let isValid = false;
-  if (ref && typeof (ref) === 'object' && !Array.isArray(ref))
-  {
-    if (ref.hasOwnProperty('type') && ref.type === Types.REFERENCE &&
-      ref.hasOwnProperty('id') && ref.hasOwnProperty('parent') &&
-      ref.hasOwnProperty('ref'))
+    static isValid(ref)
     {
-      isValid = true;
+        let isValid = false;
+        if (ref && typeof (ref) === 'object' && !Array.isArray(ref))
+        {
+            if (ref.hasOwnProperty('type') && ref.type === Types.REFERENCE &&
+                ref.hasOwnProperty('id') && ref.hasOwnProperty('parent') &&
+                ref.hasOwnProperty('ref'))
+            {
+                isValid = true;
+            }
+        }
+
+        return isValid;
     }
-  }
-
-  return isValid;
-}
 }
 
-Reference.type       = Types.REFERENCE;
-const isValid        = Reference.isValid;
-module.exports       = Reference;
+Reference.type = Types.REFERENCE;
+const isValid = Reference.isValid;
+module.exports = Reference;
