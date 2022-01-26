2de522e8fa2627b551bcfb686d627bb18f0d472f
Sandro Rama Fiorini <srfiorini@users.noreply.github.com>
2022-01-26 16:41:30 -0300

    Reformated context.

--- context.js
+++ context.js
@@ -10,95 +10,95 @@ const Node = require("./node");
 
 class Context extends Node
 {
-	constructor(id, parent)
-	{
-	super(id, parent)
-	if (arguments[0] && typeof arguments[0] === "object")
-	{
-		let context = arguments[0];
-		this.id = context.id || null;
-		this.parent = context.parent || null;
-		if (context.properties)
-		{
-			this.properties = context.properties;
-		}
-		if (context.metaProperties)
-		{
-			this.metaProperties = context.metaProperties;
-		}
-		if (context.interfaces)
-		{
-			this.interfaces = context.interfaces;
-		}
-	}
+    constructor(id, parent)
+    {
+        super(id, parent)
+        if (arguments[0] && typeof arguments[0] === "object")
+        {
+            let context = arguments[0];
+            this.id = context.id || null;
+            this.parent = context.parent || null;
+            if (context.properties)
+            {
+                this.properties = context.properties;
+            }
+            if (context.metaProperties)
+            {
+                this.metaProperties = context.metaProperties;
+            }
+            if (context.interfaces)
+            {
+                this.interfaces = context.interfaces;
+            }
+        }
 
-	else
-	{
-		this.id = id || null;
-		this.parent = parent || null;
-	}
-	this.type = Types.CONTEXT;
-}
+        else
+        {
+            this.id = id || null;
+            this.parent = parent || null;
+        }
+        this.type = Types.CONTEXT;
+    }
 
-addInterface(key, type, properties)
-{
-	if (!this.interfaces)
-	{
-		this.interfaces = {};
-	}
+    addInterface(key, type, properties)
+    {
+        if (!this.interfaces)
+        {
+            this.interfaces = {};
+        }
 
-	this.interfaces[key] = {
-		type: type,
-		properties: properties
-	};
-}
+        this.interfaces[key] = {
+            type: type,
+            properties: properties
+        };
+    }
 
-serialize()
-{
-	let context = {
-		id: this.id,
-		type: Types.CONTEXT,
-		parent: this.parent || null
-	};
+    serialize()
+    {
+        let context = {
+            id: this.id,
+            type: Types.CONTEXT,
+            parent: this.parent || null
+        };
 
-	if (this.properties)
-	{
-		context.properties = this.serializeProperties();
-	}
+        if (this.properties)
+        {
+            context.properties = this.serializeProperties();
+        }
 
-	if (this.metaProperties)
-	{
-		context.metaProperties = this.serializeMetaProperties();
-	}
+        if (this.metaProperties)
+        {
+            context.metaProperties = this.serializeMetaProperties();
+        }
 
-	if (this.interfaces)
-	{
-		context.interfaces = {};
-		for (let k in this.interfaces)
-		{
-			context.interfaces[k] = JSON.parse(JSON.stringify(this.interfaces[k])); // Lazy job
-		}
-	}
+        if (this.interfaces)
+        {
+            context.interfaces = {};
+            for (let k in this.interfaces)
+            {
+                context.interfaces[k] = JSON.parse(JSON.stringify(this.interfaces[k])); // Lazy job
+            }
+        }
 
-	return context;
-}
+        return context;
+    }
 
 
 
-static isValid(entity)
-{
-	let isValid = false;
-	if (entity && typeof (entity) === 'object' && !Array.isArray(entity))
-	{
-		if (entity.hasOwnProperty('type') && entity.type === Types.CONTEXT &&
-			entity.hasOwnProperty('id') && entity.hasOwnProperty('parent'))
-		{
-			isValid = true;
-		}
-	}
+    static isValid(entity)
+    {
+        let isValid = false;
+        if (entity && typeof (entity) === 'object' && !Array.isArray(entity))
+        {
+            if (entity.hasOwnProperty('type') && entity.type === Types.CONTEXT &&
+                entity.hasOwnProperty('id') && entity.hasOwnProperty('parent'))
+            {
+                isValid = true;
+            }
+        }
 
-	return isValid;
-}
+        return isValid;
+    }
 
 }
 
