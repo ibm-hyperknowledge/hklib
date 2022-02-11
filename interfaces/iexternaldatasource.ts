7cd3dda7cc004c8359a2d3e04c6287cc4d093a82
Marcelo Machado <marcelo.oc.machado@gmail.com>
2022-02-11 06:06:26 -0300

    Adding method to get entity in IExternalDatasource.

--- interfaces/iexternaldatasource.ts
+++ interfaces/iexternaldatasource.ts
@@ -3,8 +3,9 @@
 import HKEntity  from "../hkentity";
 
 export interface IExternalDatasource {
-  endpoint: String;
+  endpoint: string;
   executeQuery(query: string, options: any): Promise<any>;
   transform(data?: any): Promise<Map<string, HKEntity>>;
   getProperties(property?: string[]): any;
+  getEntity(key: string, options: any): Promise<any>;
 }
--- types/interfaces/iexternaldatasource.d.ts
+++ types/interfaces/iexternaldatasource.d.ts
@@ -4,8 +4,9 @@
 */
 import HKEntity from "../hkentity";
 export interface IExternalDatasource {
-    endpoint: String;
+    endpoint: string;
     executeQuery(query: string, options: any): Promise<any>;
     transform(data?: any): Promise<Map<string, HKEntity>>;
     getProperties(property?: string[]): any;
+    getEntity(key: string, options: any): Promise<any>;
 }
