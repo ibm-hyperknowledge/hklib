13399d6b1f78a1701537c9fdefe7b17cfc3dcfe7
Marcelo Machado <marcelo.oc.machado@gmail.com>
2022-03-08 00:54:59 -0300

    Updated executeQuery method signature.

--- src/interfaces/iexternaldatasource.ts
+++ src/interfaces/iexternaldatasource.ts
@@ -5,7 +5,7 @@ import HKEntity  from "../hkentity";
 
 export interface IExternalDatasource {
   endpoint: string;
-  executeQuery(query: string, parent: HKEntity | object, options: any): Promise<any>;
+  executeQuery(query: string, parent: any, options: any): Promise<any>;
   transform(data?: any): Promise<Map<string, HKEntity>>;
   getProperties(property?: string[]): any;
   getEntity(key: string, options: any): Promise<any>;
