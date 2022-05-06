60e7985ad44a2047a333b82fe07907359d87720a
Sandro Rama Fiorini <srfiorini@users.noreply.github.com>
2022-05-06 09:19:02 -0300

    Added extra copyright statements to files.

--- src/fi/fianchor.js
+++ src/fi/fianchor.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2016-present, IBM Research
+ * Licensed under The MIT License [see LICENSE for details]
+ */
+
 const FIOperator = require("./fioperator");
 
 //const PARSER = new EBNF.Grammars.W3C.Parser(grammar);
--- src/fi/test.js
+++ src/fi/test.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2016-present, IBM Research
+ * Licensed under The MIT License [see LICENSE for details]
+ */
+
 const FI = require("./fi");
 
 function testFi(str)
