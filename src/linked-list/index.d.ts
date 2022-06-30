66c0f805e3a4b9e9687db75dbe1bc6b4e9dd305a
Gabriela Pinheiro <gpinheiro@ibm.com>
2022-06-30 16:59:03 -0300

    Added linked-list to HKLib

---
+++ src/linked-list/index.d.ts
@@ -0,0 +1,29 @@
+export namespace List{
+  export class Item {
+    prev: this
+    next: this
+    list: List<this>
+  
+    detach(): this
+    prepend<T extends this>(item: T): T
+    append<T extends this>(item: T): T
+  }
+}
+export class List<T extends List.Item> implements Iterable<T> {
+  static of<T extends List.Item>(...items: T[]): List<T>
+  static from<T extends List.Item>(items: Iterable<T>): List<T>
+
+  head: T | null
+  tail: T | null
+  size: number
+
+  constructor(...items: T[])
+  toArray(): T[]
+  prepend<T>(item: T): T
+  append<T>(item: T): T
+  [Symbol.iterator](): Iterator<T>
+}
+
+export type  Item = List.Item;
+
+
