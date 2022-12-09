cd558f551dc2a56ce172ed37a3b79362705d7d46
Raphael Melo Thiago <raphaelt@br.ibm.com>
2022-12-09 15:26:12 -0300

    fixing copyright prepend script

--- prepend-copyright-notice-d-ts.sh
+++ prepend-copyright-notice-d-ts.sh
@@ -1,6 +1,11 @@
 echo "Prepending copyright notice to files in ./types..."
 for f in $(find ./dist -type f -name "*.d.ts")
 do 
-  echo "/**\n* Copyright (c) 2016-present, IBM Research\n* Licensed under The MIT License [see LICENSE for details]\n*/\n$(cat $f)" > $f
+  echo """
+/**
+* Copyright (c) 2016-present, IBM Research
+* Licensed under The MIT License [see LICENSE for details]
+*/
+$(cat $f)" > $f
 done 
 echo "Done!"
