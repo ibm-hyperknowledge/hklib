f13444ace25fa33db2c87853c9edd6d0446c9bc8
Raphael Melo Thiago <raphaelt@br.ibm.com>
2022-12-09 15:28:07 -0300

    removing some unecessary quotes

--- prepend-copyright-notice-d-ts.sh
+++ prepend-copyright-notice-d-ts.sh
@@ -1,7 +1,7 @@
 echo "Prepending copyright notice to files in ./types..."
 for f in $(find ./dist -type f -name "*.d.ts")
 do 
-  echo """
+  echo "
 /**
 * Copyright (c) 2016-present, IBM Research
 * Licensed under The MIT License [see LICENSE for details]
